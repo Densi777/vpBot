e4e1af841fb2e78e2f2762dc74c99cc07b6f3b4c
Densi777 <Denis1995.17@icloud.com>
2018-03-05 00:07:38 +0300

    Update 0.0.2

--- bot.py
+++ bot.py
@@ -2,20 +2,28 @@
 import telebot
 import config
 from telebot import types
+import datetime
 
 bot = telebot.TeleBot(config.token)
+greetings = ('Здравствуй', 'Привет', 'Доброго времени суток')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    now = datetime.datetime.now()
+    today = now.day
+    hour = now.hour
+
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton('Главное меню'))
-    bot.send_message(message.chat.id, 'Привет!\nДавай начнём!', reply_markup=keyboard)
+    keyboard.add(*[types.KeyboardButton(choice) for choice in ['Главное меню', 'Error']])
+    msg = bot.send_message(message.chat.id, 'Привет!\nДавай начнём!', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, choice)
 
-
-@bot.message_handler(content_types=["text"])
-def temp():
-    pass
+def choice(message):
+    if message.text == 'Главное меню':
+        bot.send_message(message.chat.id, '1')
+    elif message.text == 'Error':
+        bot.send_message(message.chat.id, '2')
 
 
 if __name__ == '__main__':
