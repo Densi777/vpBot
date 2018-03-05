de04e5d045ad9b028eb59643447947eabace33f9
Densi777 <Denis1995.17@icloud.com>
2018-03-06 02:07:19 +0300

    Update 0.0.1.1

--- bot.py
+++ bot.py
@@ -1,60 +1,63 @@
 # -*- coding: utf-8 -*-
 import telebot
+import psycopg2
 import config
 import inf
 from telebot import types
 import datetime
 
 bot = telebot.TeleBot(config.token)
+conn = psycopg2.connect( host='185.228.233.139', user=den, password=root7, dbname=vpbot_accounts_db)
+cursor = conn.cursor()
 
 
 @bot.message_handler(commands=['start'])
 def mainMenu(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.row('📝Заказать', 'Корпоратив')
-    keyboard.row('💰Прайс-лист', '🔧Настройки')
-    keyboard.row(⚠'Информация', '👔О нас')
-    msg = bot.send_message(message.chat.id, '📖Главное меню:', reply_markup=keyboard)
+    keyboard.row('📝 Заказать', '🥂 Корпоратив')
+    keyboard.row('💰 Прайс-лист', '⚙ Настройки')
+    keyboard.row('ℹ Информация', '👔 О нас')
+    msg = bot.send_message(message.chat.id, '📖 Главное меню:', reply_markup=keyboard)
     bot.register_next_step_handler(msg, menu)
 
 def menu(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if message.text == '🔧Настройки':
+    if message.text == '⚙ Настройки':
         settings(message)
 
-    elif message.text == '⚠Информация':
+    elif message.text == 'ℹ Информация':
         info(message)
 
-    elif message.text == '👔О нас':
+    elif message.text == '👔 О нас':
         about(message)
 
 def settings(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.row('🏠Домашний адрес', '📚История заказов')
+    keyboard.row('🏠 Домашний адрес', '📚 История заказов')
     keyboard.row('↪Назад')
-    msg = bot.send_message(message.chat.id, '👤Аккаунт:', reply_markup=keyboard)
+    msg = bot.send_message(message.chat.id, '👤 Аккаунт:', reply_markup=keyboard)
     bot.register_next_step_handler(msg, account_info)
 
 def account_info(message):
-    if message.text == '🏠Домашний адрес':
+    if message.text == '🏠 Домашний адрес':
         pass
 
-    elif message.text == '📚История заказов':
+    elif message.text == '📚 История заказов':
         pass
 
-    elif message.text == '↪Назад':
+    elif message.text == '↪ Назад':
         mainMenu(message)
 
 
 def about(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add('↪Назад')
+    keyboard.add('↪ Назад')
     msg = bot.send_message(message.chat.id, inf.about_us, reply_markup=keyboard)
     bot.register_next_step_handler(msg, mainMenu)
 
 def info(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add('↪Назад')
+    keyboard.add('↪ Назад')
     msg = bot.send_message(message.chat.id, inf.info_text, reply_markup=keyboard)
     bot.register_next_step_handler(msg, mainMenu)
 
