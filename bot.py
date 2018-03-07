cd46ad5f15119eec78731380bb9884e83aacfa70
Densi777 <Denis1995.17@icloud.com>
2018-03-07 10:14:59 +0300

    Update 0.1.0

--- bot.py
+++ bot.py
@@ -23,10 +23,10 @@ def mainMenu(message):
 def menu(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     if message.text == '📝 Заказать':
-        pass
+        order_tobacco(message)
 
     elif message.text == '🥂 Корпоратив':
-        pass
+        party(message)
 
     elif message.text == '💰 Прайс-лист':
         price_list(message)
@@ -40,17 +40,74 @@ def menu(message):
     elif message.text == '👔 О нас':
         about(message)
 
+def order_tobacco(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row('🐒 Лёгкий', '🦍 Крепкий')
+    msg = bot.send_message(message.chat.id, 'Выберите крепкость кальяна:', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, order_cups_get)
+
+def order_cups_get(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row('1️⃣ Одна', '2️⃣ Две', '3️⃣ Три')
+    keyboard.row('🔢 Более трёх', '↪ Назад')
+    msg = bot.send_message(message.chat.id, '☕ Выберите количество чашек:', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, order_cups)
+
+def order_cups(message):
+    if message.text == '1️⃣ Одна':
+        order_set_address_get(message)
+
+    elif message.text == '2️⃣ Две':
+        order_set_address_get(message)
+
+    elif message.text == '3️⃣ Три':
+        order_set_address_get(message)
+
+    elif message.text == '🔢 Более трёх':
+        order_set_address_get(message)
+
+    elif message.text == '↪ Назад':
+        order_tobacco(message)
+
+@bot.message_handler(commands=['geophone'])
+def order_set_address_get(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    geolocation_button = types.KeyboardButton(text="📍 Местоположение", request_location=True)
+    keyboard.add(geolocation_button, '↪ Назад')
+    bot.send_message(message.chat.id, 'Напишите адрес доставки\nИли отправьте местоположение', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, order_set_address)
+
+@bot.message_handler(commands=['geophone'])
+def order_set_address(message):
+    if keyboard == geolocation_button:
+        bot.send_location(location.chat.id, 'Местоположение отправлено')
+
+    elif message.text == '↪ Назад':
+        order_cups_get(message)
+
 def price_list(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.row('🐒 Лёгкие табаки', '🦍 Крепкие табаки')
+    keyboard.row('🐒 Лёгкий', '🦍 Крепкий')
     keyboard.row('↪ Назад')
-    msg = bot.send_message(message.chat.id, '👤 Аккаунт:', reply_markup=keyboard)
-    bot.register_next_step_handler(msg, account_info)
+    msg = bot.send_message(message.chat.id, 'Выберите крепкость кальяна:', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, tobaccos_prices)
+
+def tobaccos_prices(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('↪ Назад')
+    if message.text == '🐒 Лёгкий':
+        msg = bot.send_message(message.chat.id, inf.easy_tobacco, reply_markup=keyboard)
+
+    elif message.text == '🦍 Крепкий':
+        msg = bot.send_message(message.chat.id, inf.hard_tobacco, reply_markup=keyboard)
+
+    elif message.text == '↪ Назад':
+        mainMenu(message)
 
 def settings(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row('🏠 Домашний адрес', '📚 История заказов')
-    keyboard.row('↪ Назад')
+    keyboard.row('📢 Поделиться ботом' ,'↪ Назад')
     msg = bot.send_message(message.chat.id, '👤 Аккаунт:', reply_markup=keyboard)
     bot.register_next_step_handler(msg, account_info)
 
@@ -59,6 +116,9 @@ def account_info(message):
         pass
 
     elif message.text == '📚 История заказов':
+        pass
+
+    elif message.text == '📢 Поделиться ботом':
         pass
 
     elif message.text == '↪ Назад':
@@ -76,6 +136,11 @@ def info(message):
     keyboard.add('↪ Назад')
     msg = bot.send_message(message.chat.id, inf.info_text, reply_markup=keyboard)
     bot.register_next_step_handler(msg, mainMenu)
+
+@bot.message_handler(commands=['geophone'])
+def geophone(message):
+    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
 
 
 if __name__ == '__main__':
