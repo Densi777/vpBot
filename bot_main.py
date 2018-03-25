5908b08bc1371ee80fa728629c5f177fdfbd8005
Densi777 <Denis1995.17@icloud.com>
2018-03-25 15:26:18 +0300

    Update 1.0.8

--- bot_main.py
+++ bot_main.py
@@ -142,8 +142,8 @@ def done(message):
 def close_order(message):
     if message.text == '✔ Завершить':
         if config.excount == 3:
-            cur.execute('''INSERT INTO USER_DATA VALUES (%s, %s, 'Заказ: Лёгкий кальян, 1 чаша');''',
-                        (message.chat.id, message.chat.username))
+            cur.execute('''INSERT INTO "USER_DATA" VALUES (%s, %s, 'Заказ: Лёгкий кальян, 1 чаша по адресу: %s');''',
+                        (message.chat.id, message.chat.username, inf.address))
             bot.send_message(chat_id=config.my_id, text='Заказ:\nЛёгкий кальян\n1 чаша\nПо адресу:\n' + inf.address)
 
         elif config.excount == 4:
